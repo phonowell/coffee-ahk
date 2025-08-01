2c92af91b4e24c800c2c36a1d4e7a0f7626cfa66
Mimiko <phonowell@gmail.com>
2025-08-01 19:36:43 +0800

    Refactor script/z: Remove index.ast.json and simplify index.coffee
    - Deleted the index.ast.json file as it was no longer needed.
    - Simplified index.coffee by removing all previous content and replacing it with a single function call.
    - Updated task/z.ts to include new options for the main function, enabling AST generation and verbose logging.

--- task/z.ts
+++ task/z.ts
@@ -3,9 +3,11 @@ import c2a from '../src/index.js'
 const main = async () => {
   await c2a('./script/z/index.coffee', {
     ast: true,
+    coffeeAst: true,
     comments: true,
     metadata: false,
     salt: 'ahk',
+    verbose: true,
   })
 }
 
